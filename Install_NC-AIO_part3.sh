954b182ca50c1704df35f17bbb0c8b8d8e3127f8
Dave Thompsom <32268058+OS-DaveThompson@users.noreply.github.com>
2023-04-23 02:28:05 -0700

    Update Install_NC-AIO_part3.sh

--- Install_NC-AIO_part3.sh
+++ Install_NC-AIO_part3.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
-echo 'The file: /etc/apache2/sites-available/nextcloud.conf will be populated.'
-echo
-echo '<VirtualHost *:80>' > /etc/apache2/sites-available/nextcloud.conf
-echo '    DocumentRoot "/var/www/html/nextcloud"' >> /etc/apache2/sites-available/nextcloud.conf
-echo '    ServerName example.com' >> /etc/apache2/sites-available/nextcloud.conf
-echo '' >> /etc/apache2/sites-available/nextcloud.conf
-echo '    <Directory "/var/www/html/nextcloud/">' >> /etc/apache2/sites-available/nextcloud.conf
-echo '        Options MultiViews FollowSymlinks' >> /etc/apache2/sites-available/nextcloud.conf
-echo '        AllowOverride All' >> /etc/apache2/sites-available/nextcloud.conf
-echo '        Order allow,deny' >> /etc/apache2/sites-available/nextcloud.conf
-echo '        Allow from all' >> /etc/apache2/sites-available/nextcloud.conf
-echo '    </Directory>' >> /etc/apache2/sites-available/nextcloud.conf
-echo '' >> /etc/apache2/sites-available/nextcloud.conf
-echo '    TransferLog /var/log/apache2/nextcloud_access.log' >> /etc/apache2/sites-available/nextcloud.conf
-echo '    ErrorLog /var/log/apache2/nextcloud_error.log' >> /etc/apache2/sites-available/nextcloud.conf
-echo '' >> /etc/apache2/sites-available/nextcloud.conf
-echo '</VirtualHost>' >> /etc/apache2/sites-available/nextcloud.conf
-echo '' >>/etc/apache2/sites-available/nextcloud.conf
 
+
+
+
+
+echo
+echo '###===---'
+echo '######======------'
+echo '############============------------'
+echo '###'
+echo '###   Configure Nextcloud webserver properties.'
+echo "###   Create the file /etc/apache2/sites-available/nextcloud.conf"
+echo '###   With the text from below'
+echo
+echo '<VirtualHost *:80>'
+echo '    DocumentRoot "/var/www/html/nextcloud"'
+echo '    ServerName example.com'
+echo
+echo '    <Directory "/var/www/html/nextcloud/">'
+echo '        Options MultiViews FollowSymlinks'
+echo '        AllowOverride All'
+echo '        Order allow,deny'
+echo '        Allow from all'
+echo '    </Directory>'
+echo
+echo '    TransferLog /var/log/apache2/nextcloud_access.log'
+echo '    ErrorLog /var/log/apache2/nextcloud_error.log'
+echo
+echo '</VirtualHost>'
