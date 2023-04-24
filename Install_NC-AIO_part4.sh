0aff1f013ebe5cf23e946cd1a7ae572fe77f811a
Dave Thompsom <32268058+OS-DaveThompson@users.noreply.github.com>
2023-04-23 19:46:48 -0700

    Update Install_NC-AIO_part4.sh

--- Install_NC-AIO_part4.sh
+++ Install_NC-AIO_part4.sh
@@ -1,2 +1,21 @@
+#!/bin/bash
 
-sudo a2ensite nextcloud.conf
+echo 'sudo nano /etc/php/8.1/apache2/php.ini'
+echo
+echo 'max_execution_time = 360'
+echo 'memory_limit = 512M'
+echo 'post_max_size = 200M'
+echo 'upload_max_filesize = 200M'
+echo 'date.timezone = Europe/London'
+echo 'opcache.enable=1'
+echo 'opcache.memory_consumption=128'
+echo 'opcache.interned_strings_buffer=8'
+echo 'opcache.max_accelerated_files=10000'
+echo 'opcache.revalidate_freq=1'
+echo 'opcache.save_comments=1'
+echo
+echo 'After modifying php.ini, run the following:'
+echo 'a2enmod dir env headers mime rewrite ssl'
+echo 'systemctl restart apache2'
+echo 'systemctl status apache2'
+
