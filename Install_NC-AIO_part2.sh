1da1cbde67c8298a3edfd4b521057b880d37dd25
Dave Thompsom <32268058+OS-DaveThompson@users.noreply.github.com>
2023-04-23 19:46:57 -0700

    Update Install_NC-AIO_part2.sh

--- Install_NC-AIO_part2.sh
+++ Install_NC-AIO_part2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 apt install php -y
-apt install php-apcu php-bcmath php-cli php-common php-curl php-gd php-gmp php-imagick php-intl php-mbstring php-mysql php-zip php-xml phpenmod bcmath gmp imagick intl -y
+apt install php-apcu php-bcmath php-cli php-common php-curl php-gd php-gmp php-imagick php-intl php-mbstring php-mysql php-zip php-xml -y
+sudo phpenmod bcmath gmp imagick intl
 apt install unzip -y
 apt install libmagickcore-6.q16-6-extra -y
 
